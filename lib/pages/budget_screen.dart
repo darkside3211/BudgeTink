930d342b6199ad4fb9c119c13b3691147e9202b8
Gerard Dominic Baldo <baldo.251131@sjdelmonte.sti.edu.ph>
2023-12-11 21:27:50 +0800

    added a very basic implementation of budget computation

--- lib/pages/budget_screen.dart
+++ lib/pages/budget_screen.dart
@@ -1,16 +1,61 @@
 import 'package:flutter/material.dart';
 
-class BudgetScreen extends StatelessWidget {
+class BudgetScreen extends StatefulWidget {
   const BudgetScreen({super.key});
 
   @override
+  _BudgetScreenState createState() => _BudgetScreenState();
+}
+
+class _BudgetScreenState extends State<BudgetScreen> {
+  double income = 0.0;
+  double expenses = 0.0;
+  double savings = 0.0;
+
+  void updateIncome(double value) {
+    setState(() {
+      income = value;
+    });
+  }
+
+  void updateExpenses(double value) {
+    setState(() {
+      expenses = value;
+    });
+  }
+
+  void updateSavings() {
+    setState(() {
+      savings = income - expenses;
+    });
+  }
+
+  @override
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(255, 204, 22, 189),
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 22, 125, 165),
       body: Center(
-        child: Text(
-          'Budget',
-          style: TextStyle(fontSize: 24, color: Colors.white),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onChanged: (value) {
+                updateIncome(double.parse(value));
+                updateSavings();
+              },
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Income'),
+            ),
+            TextField(
+              onChanged: (value) {
+                updateExpenses(double.parse(value));
+                updateSavings();
+              },
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Expenses'),
+            ),
+            Text('Savings: \$${savings.toStringAsFixed(2)}'),
+          ],
         ),
       ),
     );

2b853bbef26779dfe0d2e53e20aea459c65a135f
Gerard Dominic Baldo <baldo.251131@sjdelmonte.sti.edu.ph>
2023-12-16 21:02:14 +0800

    added a calendar with a heat map but its still not finished. will continue soon

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:animations/animations.dart';
+import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+DateTime displayedMonth = DateTime.now();
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
+    Map<DateTime, int> data = {
+      displayedMonth.subtract(Duration(days: 3)): 5,
+      displayedMonth.subtract(Duration(days: 2)): 35,
+      displayedMonth.subtract(Duration(days: 1)): 14,
+      displayedMonth: 5,
+    };
+
+    Map<int, Color> colorsets = {
+      1: Colors.green[100]!,
+      10: Colors.green[300]!,
+      30: Colors.green[500]!,
+    };
     return WillPopScope(
       onWillPop: () async => false, // Disable back button
       child: Scaffold(
@@ -78,6 +98,28 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
               ],
+            ),
+            Gap(20), //adds a gap of 20 pixels
+            GestureDetector(
+              onHorizontalDragEnd: (DragEndDetails details) {
+                if (details.velocity.pixelsPerSecond.dx > 0) {
+                  // Swiped right, go to previous month
+                  setState(() {
+                    displayedMonth =
+                        DateTime(displayedMonth.year, displayedMonth.month - 1);
+                  });
+                } else if (details.velocity.pixelsPerSecond.dx < 0) {
+                  // Swiped left, go to next month
+                  setState(() {
+                    displayedMonth =
+                        DateTime(displayedMonth.year, displayedMonth.month + 1);
+                  });
+                }
+              },
+              child: HeatMapCalendar(
+                datasets: data,
+                colorsets: colorsets,
+              ),
             ),
           ],
         ),

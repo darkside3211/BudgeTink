0b8a946f1532e248df721332347ba382992fafe0
Gerard Dominic Baldo <baldo.251131@sjdelmonte.sti.edu.ph>
2024-02-26 10:17:51 +0800

    added functionality to the records page which displays all the savings the user has made from the database record

--- lib/pages/records.screen.dart
+++ lib/pages/records.screen.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class RecordsScreen extends StatelessWidget {
-  const RecordsScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(255, 96, 62, 219),
-      body: Center(
-        child: Text(
-          'Records',
-          style: TextStyle(fontSize: 24, color: Colors.white),
-        ),
-      ),
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection('users')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .collection('savings')
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return ListView(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+            return ListTile(
+              title: Text("Date: ${document.id}"),
+              subtitle: Text("Amount: ${data['amount']}"),
+            );
+          }).toList(),
+        );
+      },
     );
   }
 }
